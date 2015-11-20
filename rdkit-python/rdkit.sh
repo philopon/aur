a243899c8c8c13423d17b753768b4d923764a6e8
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2015-11-20 20:12:38 +0900

    rdkit-python: add load script

---
+++ rdkit-python/rdkit.sh
@@ -0,0 +1,3 @@
+export RDBASE=/opt/rdkit-python
+export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$RDBASE/lib/python3.5/site-packages:$PYTHONPATH
---
+++ rdkit-python2/rdkit.sh
@@ -0,0 +1,3 @@
+export RDBASE=/opt/rdkit-python2
+export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$RDBASE/lib/python2.7/site-packages:$PYTHONPATH
