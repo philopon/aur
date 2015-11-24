879ddb08ba6365678971c63d5f8e100236f4b1fa
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2015-11-24 17:23:30 +0900

    update rdkit-python

--- rdkit-python/rdkit.sh
+++
@@ -1,3 +0,0 @@
-export RDBASE=/opt/rdkit-python
-export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$RDBASE/lib/python3.5/site-packages:$PYTHONPATH
--- rdkit-python2/rdkit.sh
+++
@@ -1,3 +0,0 @@
-export RDBASE=/opt/rdkit-python2
-export LD_LIBRARY_PATH=$RDBASE/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$RDBASE/lib/python2.7/site-packages:$PYTHONPATH
